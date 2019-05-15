d961786317e4f9762e8845cacbf18920c31e90af
wangjianping <wangjianping@pyhtech.cn>
2019-05-15 11:38:56 +0800

    dev3.0.0版本代码

---
+++ app/src/main/java/com/wjp/test_git/Test2.java
@@ -0,0 +1,12 @@
+package com.wjp.test_git;
+
+/**
+ * @author wangjianping
+ * @date 2019/5/15 11:37
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        //新增一个类
+        int b = 2;
+    }
+}
